a1e8cef2a5731b55b493ee24ae236e5b331c32a2
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-02-25 10:22:10 +0530

    Update ItemCountSummary.js

--- frontend/src/components/ItemCountSummary.js
+++ frontend/src/components/ItemCountSummary.js
@@ -11,7 +11,7 @@ const ItemCountSummary = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/selects");
+      const response = await axios.get("http://18.231.150.111:5000/selects");
       setData(response.data);
     } catch (error) {
       console.log("Error fetching data: ", error);
